fcf8172043ecdc924de2631ec5d9e118f4fa74d0
byeol <jeongeunbyeol8@gmail.com>
2022-09-22 17:54:19 +0900

    일차 룰렛

--- Roulette.js
+++ Roulette.js
@@ -4,62 +4,25 @@ let bgImg = document.getElementById("bgImg");
 function changeBackground() {
   let index = changeBg.selectedIndex;
   bgImg.src = changeBg.options[index].value;
-  //   console.log(changeBg.selectedIndex);
-  //   let rouletteSize = changeBg.selectedIndex + 2;
-  //   console.log(rouletteSize);
 }
 
 changeBackground();
 changeBg.onchange = changeBackground;
 
-function rotateDefault(plus) {
-  //   const ran = Math.floor(Math.random() * rouletteSize);
-  //   const rotate = ran * arc + 3240 + arc * 3;
-
+function rotateDefault() {
   let rotateImage = document.getElementById("bgImg");
   rotateImage.style.transition = "transform 1s";
-  let rotate = (rotateImage.style.transform = "rotate(8turn)");
-  rotate();
-}
 
-function rotateDiverse() {
   let rouletteSize = changeBg.selectedIndex + 2;
   let arc = 360 / rouletteSize;
-  rotateDefault();
-
-  let image = document.getElementById("bgImg");
-  let plus = (image.style.transform = "rotate((90deg)");
-
-  plus();
+  let defaultNum = 360 * 8;
+  let totalNum = defaultNum + arc;
+  rotateImage.style.transform = "rotate(" + totalNum + "deg)";
 }
 
 function final() {
-  //   rotateDefault();
-  rotateDiverse();
+  rotateDefault();
   //   alert("test");
 }
 
-// document.getElementById("roulette-button").onclick = rotateDefault;
 document.getElementById("roulette-button").onclick = final;
-// document.getElementById("roulette-button").onclick = rotateDiverse;
-
-// function rotateImg() {
-//   document.getElementById("bgImg").getElementsByClassName.transform =
-//     "rotate(90deg)";
-// }
-
-// function startBtn() {
-//   document.getElementsById("bgImg").style.animation = "";
-//   var randomRotate = Math.random() * 3000 + 1000;
-//   var randomAnimation = Math.random() * 7000 + 2000;
-
-//   document
-//     .getElementById("bgImg")
-//     .animate(
-//       [
-//         { transform: "rotate(0deg)" },
-//         { transform: "rotate(" + randomRotate + "deg)" },
-//       ],
-//       { fill: "forwards", duration: randomAnimation, easing: "ease-in-out" }
-//     );
-// }
