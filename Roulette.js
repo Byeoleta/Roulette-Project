463be7217bf18e77a3e0dc275e6008b39356b8c5
byeol <jeongeunbyeol8@gmail.com>
2022-09-22 13:35:15 +0900

    옵션 따라 칸수 설정

--- Roulette.js
+++ Roulette.js
@@ -4,28 +4,36 @@ let bgImg = document.getElementById("bgImg");
 function changeBackground() {
   let index = changeBg.selectedIndex;
   bgImg.src = changeBg.options[index].value;
+  //   console.log(changeBg.selectedIndex);
+  //   let rouletteSize = changeBg.selectedIndex + 2;
+  //   console.log(rouletteSize);
 }
 
 changeBackground();
 changeBg.onchange = changeBackground;
+
+function rotateBackground() {
+  let rouletteSize = changeBg.selectedIndex + 2;
+  console.log(rouletteSize);
+}
 
 // function rotateImg() {
 //   document.getElementById("bgImg").getElementsByClassName.transform =
 //     "rotate(90deg)";
 // }
 
-function startBtn() {
-  document.getElementsById("bgImg").style.animation = "";
-  var randomRotate = Math.random() * 3000 + 1000;
-  var randomAnimation = Math.random() * 7000 + 2000;
+// function startBtn() {
+//   document.getElementsById("bgImg").style.animation = "";
+//   var randomRotate = Math.random() * 3000 + 1000;
+//   var randomAnimation = Math.random() * 7000 + 2000;
 
-  document
-    .getElementById("bgImg")
-    .animate(
-      [
-        { transform: "rotate(0deg)" },
-        { transform: "rotate(" + randomRotate + "deg)" },
-      ],
-      { fill: "forwards", duration: randomAnimation, easing: "ease-in-out" }
-    );
-}
+//   document
+//     .getElementById("bgImg")
+//     .animate(
+//       [
+//         { transform: "rotate(0deg)" },
+//         { transform: "rotate(" + randomRotate + "deg)" },
+//       ],
+//       { fill: "forwards", duration: randomAnimation, easing: "ease-in-out" }
+//     );
+// }
