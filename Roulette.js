5275c852a6ec4a065c5b9ad1411dc0b3cd4967ad
byeol <jeongeunbyeol8@gmail.com>
2022-09-22 11:49:54 +0900

    start button

--- Roulette.js
+++ Roulette.js
@@ -8,3 +8,24 @@ function changeBackground() {
 
 changeBackground();
 changeBg.onchange = changeBackground;
+
+// function rotateImg() {
+//   document.getElementById("bgImg").getElementsByClassName.transform =
+//     "rotate(90deg)";
+// }
+
+function startBtn() {
+  document.getElementsById("bgImg").style.animation = "";
+  var randomRotate = Math.random() * 3000 + 1000;
+  var randomAnimation = Math.random() * 7000 + 2000;
+
+  document
+    .getElementById("bgImg")
+    .animate(
+      [
+        { transform: "rotate(0deg)" },
+        { transform: "rotate(" + randomRotate + "deg)" },
+      ],
+      { fill: "forwards", duration: randomAnimation, easing: "ease-in-out" }
+    );
+}
