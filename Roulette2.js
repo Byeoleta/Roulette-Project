a932d2918bf154e3cf37c1ef2cc2cd82135b33f6
byeol <jeongeunbyeol8@gmail.com>
2022-09-28 10:13:51 +0900

    pointer 위치 설정가능, 룰렛 다중호출 가능

--- Roulette2.js
+++ Roulette2.js
@@ -2,37 +2,66 @@ function Roulette(el, config) {
   this.el = el;
   this.config = config;
   this.config.win = config.win - 1;
+  this.start = this.config.start || "top";
+  this.defaultRotate = this.config.rotate * 360 || 10 * 360;
+  this.rotateDuration = this.config.duration || "3000";
 
   //element 초기값 설정
-  this.button = null;
   this.roulette = $(this.el);
+  this.button = this.config.button
+    ? $(this.config.button.el)
+    : this.roulette.children(".roulette-button");
+  this.rouletteArrow = this.roulette.children(".arrow");
   this.rouletteBox = this.roulette.children(".roulette-container");
   this.rouletteCircle = this.rouletteBox.children(".circle");
-
-  this.defaultRotate = 10 * 360;
-  this.rotateDuration = 4000;
+  this.startDeg = 0;
 
   //룰렛 셋팅
   this.init = function () {
     this.setRoulette();
-    this.config.button !== undefined && this.setButton();
+    this.setStartRoulette();
+    this.setButton();
   };
 
   // 룰렛 설정
   this.setRoulette = function () {
     this.rouletteCircle.attr(
       "src",
-      "./assets/bg_circle" + this.config.space + ".png"
+      "./static/bg_circle" + this.config.space + ".png"
     );
   };
 
   // 버튼 설정
   this.setButton = function () {
     var _self = this;
-    this.button = $(this.config.button.el);
-    this.button.on("click", function () {
-      _self.rotateRoulette(_self.getRouletteRotate());
+    this.button.click(function () {
+      _self.rotateRoulette(_self.getRouletteRotate() - _self.startDeg);
     });
+  };
+
+  //start 지점 셋팅
+  this.setStartRoulette = function () {
+    var angle = 90;
+    var startPoint = 0;
+    switch (this.start) {
+      case "top":
+        startPoint = 0;
+        break;
+      case "right":
+        startPoint = 1;
+        break;
+      case "bottom":
+        startPoint = 2;
+        break;
+      case "left":
+        startPoint = 3;
+        break;
+    }
+
+    this.startDeg = angle * startPoint;
+
+    this.rouletteCircle.css("rotate", angle * startPoint + "deg");
+    this.rouletteArrow.addClass(this.start);
   };
 
   //룰렛 돌리기
